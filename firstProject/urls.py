7beaa63fc132dabdbf073b383b3dd097f43ba1c0
Hyung Tak Jin <njir@naver.com>
2017-06-29 04:15:12 +0000

    feat: Add url and html

--- firstProject/urls.py
+++ firstProject/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')), #http://127.0.0.1:8000/ 로 들어오는 모든 접속 요청을 blog.urls로 전송해 추가 명령
 ]
